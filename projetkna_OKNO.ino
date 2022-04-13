053581953885ef4d7bbdb949642415d35a8bb32a
Kosi18 <81773870+Kosi18@users.noreply.github.com>
2022-04-13 11:38:20 +0200

    Add files via upload

---
+++ projetkna_OKNO.ino
@@ -0,0 +1,49 @@
+int senzorO= 1;
+int senzorZ= 2;
+int senzorP= 3;
+int odpiranje= 4;
+int zapiranje= 5;
+int polovica= 6;
+int LED= 7;
+int LED2= 8;
+int LED3= 9;
+
+void setup() {
+  pinMode(senzorO, INPUT);
+  pinMode(senzorZ, INPUT);
+  pinMode(senzorP, INPUT);
+  pinMode(odpiranje, INPUT);
+  pinMode(zapiranje, INPUT);
+  pinMode(polovica, INPUT);
+  pinMode(LED, OUTPUT );
+  pinMode(LED2, OUTPUT );
+  pinMode(LED3, OUTPUT );
+  Serial.begin(9600);
+}
+void loop() {
+   
+  //če je pritisnjena odpiranje sveti LED, če pritisnemo senzor odprt se led ugasne
+  if(digitalRead(odpiranje)==1)
+      digitalWrite(LED,1);
+      }
+  if(digitalRead(senzorO)==1)
+      digitalWrite(LED,0);
+      }
+      
+  //če je pritisnjen zapiranje sveti LED2, če pritisnemo senzor zaprt se led ugasne
+  if(digitalRead(zapiranje)==1){
+      digitalWrite(LED2,1);
+      }
+  if(digitalRead(senzorZ)==1){
+      digitalWrite(LED2,0);
+      }
+
+  //če je pritisnjen polovica sveti LED3, če pritisnemo senzor polovica se led ugasne
+  if(digitalRead(polovica)==1){
+      digitalWrite(LED3,1);
+      }
+  if(digitalRead(senzorP)==1){
+      digitalWrite(LED3,0);
+      }
+}
+     
